5f383979fb588690d849f95538261f5cfc3976a0
Aurora-N <Aurorallz@outlook.com>
2024-01-22 13:06:25 +0800

    Site updated: 2024-01-22 13:06:25

--- js/toc.js
+++ js/toc.js
@@ -8,3 +8,19 @@
     });
   });
 })();
+
+// hexo.extend.tag.register('toc', function(args, content) {
+//   const headings = document.querySelectorAll('.content h1, .content h2, .content h3, .content h4, .content h5, .content h6');
+//   let toc = '<ul>';
+
+//   headings.forEach((heading, index) => {
+//     const id = heading.id || `toc-${index}`;
+//     heading.id = id;
+
+//     const level = parseInt(heading.tagName.charAt(1), 10);
+//     toc += `<li class="toc-level-${level}"><a href="#${id}">${heading.textContent}</a></li>`;
+//   });
+
+//   toc += '</ul>';
+//   return { toc: toc };
+// });
